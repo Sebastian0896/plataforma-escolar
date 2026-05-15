58e19290c1f237a4745baf88ff13d502f6514cd4
Sebastián González Rodríguez <sebagonzalez10@outlook.es>
2026-05-15 13:37:58 -0400

    Actualizmaos lib/prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,24 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+import 'dotenv/config'
 
-const connectionString = `${process.env.DATABASE_URL}`;
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@/generated/prisma/client'
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const connectionString = process.env.DATABASE_URL!
 
-export { prisma };
+const adapter = new PrismaPg({
+  connectionString,
+})
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
